f2553e4dfb691b79a00620f5d2f07b806ad3c68d
Anthony Marakis <antmarakis@programmers.gr>
2018-02-18 00:56:36 +0200

    Update spooky.py

--- Spooky Authors/spooky.py
+++ Spooky Authors/spooky.py
@@ -6,8 +6,7 @@ from utils import *
 train_x = pd.read_csv("train.csv", sep=',')
 test_x = pd.read_csv("test.csv", sep=',')
 
-parse_text(train_x)
-EAP, HPL, MWS = read_from_file(['eap.txt', 'hpl.txt', 'mws.txt'])
+EAP, HPL, MWS = parse_text(train_x)
 c_eap, c_hpl, c_mws = create_dist(EAP), create_dist(HPL), create_dist(MWS)
 
 dist = {'EAP': c_eap, 'HPL': c_hpl, 'MWS': c_mws}
